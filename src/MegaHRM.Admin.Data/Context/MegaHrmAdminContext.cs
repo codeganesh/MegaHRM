aa39a0aeca84c4a238574b5e9aeac12c749f8393
codeganesh <codeganesh@gmail.com>
2018-12-16 04:22:39 -0800

    Added context class

---
+++ src/MegaHRM.Admin.Data/Context/MegaHrmAdminContext.cs
@@ -0,0 +1,15 @@
+﻿using MegaHRM.Admin.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaHRM.Admin.Data.Context
+{
+    public class MegaHrmAdminContext : IdentityDbContext<MegaHrmUser,MegaHrmRole,int>
+    {
+        public MegaHrmAdminContext(DbContextOptions<MegaHrmAdminContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
